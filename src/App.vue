6e6d77a49624d25d7dcb220f0c42a370a3b39a79
Jonathan Bahena <bahena@Jonathans-MacBook-Air.local>
2021-07-21 22:25:32 -0500

    hw done

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Chalkboard;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,93 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 id="stuff2">{{ message }}</h1>
+    <div v-for="place in places" v-bind:key="place.id">
+      <h2 id="stuff">{{ place.name }}</h2>
+      <h4 id="stuff2">{{ place.address }}</h4>
+      <button v-on:click="showPlace(place)">More info!</button>
+    </div>
+    <div>
+      Name:
+      <input type="text" v-model="newPlacesParams.name" />
+      Address:
+      <input type="text" v-model="newPlacesParams.address" />
+      <button v-on:click="showPlace(place)">More info!</button>
+    </div>
+    <button v-on:click="createPlace">Create place</button>
+    <div>
+      <dialog id="place-details">
+        <form method="dialog">
+          <h1>Place Info!</h1>
+          <p>
+            Name:
+            <input type="text" v-model="currentPlace.name" />
+          </p>
+          <p>
+            Address:
+            <input type="text" v-model="currentPlace.address" />
+          </p>
+          <button v-on:click="updatePlace(currentPlace)">Update Place</button>
+          <button>Close</button>
+        </form>
+      </dialog>
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "You Are Viewing Places In Chicago!!!",
+      places: [],
+      newPlacesParams: {},
+      currentPlace: {},
+    };
+  },
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios.get("http://localhost:3000/places").then((response) => {
+        this.places = response.data;
+        console.log("All Places:", this.places);
+      });
+    },
+    createPlace: function () {
+      console.log("testing");
+      axios
+        .post("http://localhost:3000/places", this.newPlacesParams)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.place.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    showPlace: function (place) {
+      console.log(place);
+      this.currentPlace = place;
+      document.querySelector("#place-details").showModal();
+    },
+    updatePlace: function (place) {
+      var editPlaceParams = place;
+      axios
+        .patch("http://localhost:3000/places/" + place.id, editPlaceParams)
+        .then((response) => {
+          console.log("Success!", response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
   },
 };
 </script>
+
+<style>
+#stuff {
+  color: #ff0000;
+}
+#stuff2 {
+  color: #00a708;
+}
+</style>
